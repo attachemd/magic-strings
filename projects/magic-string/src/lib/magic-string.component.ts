735026eafbdedebc129598b3956e558d3f5d80b7
attachemd <mattache.vivace@gmail.com>
2022-01-17 17:20:00 +0100

    Angular library implemented.

--- projects/magic-string/src/lib/magic-string.component.ts
+++ projects/magic-string/src/lib/magic-string.component.ts
@@ -1,20 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'ft-magic-string',
   template: `
-    <p>
-      magic-string works!
-    </p>
+    <input type="text" (keydown.enter)="markText($event)">
+    <div #content [hidden]="true">
+      <ng-content></ng-content>
+    </div>
+    <div [innerHTML]="controlledContent"></div>
   `,
   styles: [
-  ]
+    `.mark {
+      background-color: yellow;
+    }`
+  ],
+  encapsulation: ViewEncapsulation.None
 })
 export class MagicStringComponent implements OnInit {
 
+  @ViewChild('content', { static: true }) content:ElementRef = {} as ElementRef;
+  originalContent:String= "";
+  controlledContent:String= "";
   constructor() { }
 
   ngOnInit(): void {
+    this.controlledContent = this.originalContent = this.content.nativeElement.textContent;
   }
 
+  markText($event:any) {
+    let value = ($event.target as HTMLInputElement).value;
+    this.controlledContent = this.originalContent
+    this.controlledContent = this.originalContent
+      .replace(
+        new RegExp(value, 'g'),
+        `<span class="mark">${value}</span>`
+      )
+    console.log(value)
+  }
 }
--- projects/magic-strings-example/src/app/app.module.ts
+++ projects/magic-strings-example/src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import {MagicStringModule} from "magic-string";
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MagicStringModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
